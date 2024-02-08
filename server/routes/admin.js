b3559f90b37597e982ece257b737d1aabd0ce9ea
SubhamSST <tripathysubham4@gmail.com>
2024-02-09 02:09:13 +0530

    new project

---
+++ server/routes/admin.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const router = express.Router();
+const User=require('../models/user');
+const adminLayout = '../views/layouts/admin';
+
+
+
+
+
+
+
+
+
+module.exports = router;
